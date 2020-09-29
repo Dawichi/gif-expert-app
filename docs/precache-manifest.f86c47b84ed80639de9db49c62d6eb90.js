f3a2eab90c1fd590076336176832a6334d480cf7
David M. Fajardo <davidmf99@gmail.com>
2020-09-30 00:12:16 +0200

    docs agregado

---
+++ docs/precache-manifest.f86c47b84ed80639de9db49c62d6eb90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf0749aefe431e2d69cbe88248692075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b3a0f5c91b151f0c54",
+    "url": "/static/css/main.2c3c1926.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b3a0f5c91b151f0c54",
+    "url": "/static/js/main.b46fcd31.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
